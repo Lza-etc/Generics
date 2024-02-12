bda6990078a60a55c5523bf20922d896e29543f9
Lza-etc <emj688531@gmail.com>
2024-02-12 16:34:14 +0530

    Initial commit

---
+++ Generics/GenericStack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class GenericStack<T> where T : struct
+    {
+        readonly List<T> stack = new();
+        public void Push(T item)
+        {
+            if (!typeof(T).IsValueType)
+                throw new InvalidOperationException("Only value types are allowed.");
+            stack.Add(item);
+        }
+        public void Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("The stack is empty.");
+            stack.RemoveAt(stack.Count - 1);
+        }
+        public T Peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("The stack is empty.");
+            return stack[^1];
+        }
+        public bool IsEmpty()
+        {
+            return stack.Count == 0; 
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,74 @@
+﻿using Generics;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        GenericStack<int> stack = new();
+        char choice;
+
+        do
+        {
+            Console.WriteLine("Generic Stack Operations");
+            Console.WriteLine("1.Push");
+            Console.WriteLine("2.Pop");
+            Console.WriteLine("3.Peek");
+            Console.WriteLine("4.IsEmpty");
+            Console.WriteLine("5.Exit");
+            Console.WriteLine("Enter your choice");
+            choice = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            try
+            {
+                switch (choice)
+                {
+                    case '1':
+                        Console.Write("Enter the item: ");
+                        if (int.TryParse(Console.ReadLine(), out int item))
+                        {
+                            stack.Push(item);
+                        }
+                        break;
+                    case '2':
+                        if (!stack.IsEmpty())
+                        {
+                            stack.Pop();
+                            Console.WriteLine("Top element deleted");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Stack is empty. No top element.");
+                        }
+                        break;
+                    case '3':
+                        if (!stack.IsEmpty())
+                        {
+                            int topElement = stack.Peek();
+                            Console.WriteLine($"Top element in stack is: {topElement}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Stack is empty. No top element.");
+                        }
+                        break;
+                    case '4':
+                        Console.WriteLine($"The stack is empty: {stack.IsEmpty()}");
+                        break;
+                    case '5':
+                        Console.WriteLine("Exited successfully");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice");
+                        break;
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine($"{e.Message}");
+            }
+
+        } while (choice != '5');
+    }
+}
